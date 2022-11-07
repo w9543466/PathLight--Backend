6a92d15f2d2731ed80ed48c1121201f5fb5aec6c
w9543466 <w9543466@live.tees.ac.uk>
2022-11-07 12:22:10 +0000

    Initial commit

---
+++ src/main/java/uk/ac/tees/w9543466/pathlight/PathLightApplication.java
@@ -0,0 +1,12 @@
+package uk.ac.tees.w9543466.pathlight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PathLightApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PathLightApplication.class, args);
+    }
+}
---
+++ src/test/java/uk/ac/tees/w9543466/pathlight/PathLightApplicationTests.java
@@ -0,0 +1,13 @@
+package uk.ac.tees.w9543466.pathlight;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PathLightApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
